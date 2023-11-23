932878aa2a9810c5a60c652a7fc73b2860e9b833
Luiz Cezer <lccezinha@gmail.com>
2023-11-23 15:29:19 -0300

    Start with channel

---
+++ islands_interface/lib/islands_interface/user_socket.ex
@@ -0,0 +1,9 @@
+defmodule IslandsInterface.UserSocket do
+  use Phoenix.Socket
+
+  channel "game:*", IslandsInterfaceWeb.GameChannel
+
+  def connect(_params, socket) do
+    {:ok, socket}
+  end
+end
--- islands_interface/lib/islands_interface_web/endpoint.ex
+++ islands_interface/lib/islands_interface_web/endpoint.ex
@@ -12,6 +12,7 @@ defmodule IslandsInterfaceWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/socket", IslandsInterface.UserSocket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
